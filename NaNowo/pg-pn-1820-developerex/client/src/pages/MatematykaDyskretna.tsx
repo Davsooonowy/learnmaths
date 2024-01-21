55401ec4de3402410c7a7d45cfc046be26f015dc
HadesXBloodFire <mwilewski@student.agh.edu.pl>
2024-01-21 18:04:26 +0100

    podstronki

---
+++ NaNowo/pg-pn-1820-developerex/client/src/pages/MatematykaDyskretna.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+const MatematykaDyskretna = () => {
+    return (
+        <Card className="w-full sm:w-3/4 lg:w-1/2 border-0 shadow-none mx-auto">
+            <CardHeader className="flex-row items-center justify-between">
+                <CardTitle className="text-3xl text-center">Matematyka Dyskretna</CardTitle>
+            </CardHeader>
+            <CardContent>
+                {/* Your content here */}
+            </CardContent>
+        </Card>
+    );
+};
+
+export default MatematykaDyskretna;
