2605f11dfdeebf604f27f47f8533f9f42772a092
HadesXBloodFire <mwilewski@student.agh.edu.pl>
2024-01-21 17:37:07 +0100

    Kafelki wykładowcy, responsywność

--- NaNowo/pg-pn-1820-developerex/client/src/main.tsx
+++ NaNowo/pg-pn-1820-developerex/client/src/main.tsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Room from './pages/Room';
 import { Provider } from 'jotai';
 import { ThemeProvider } from '@/components/theme-provider';
 import Registration from "@/pages/Register.tsx";
@@ -26,14 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                                 element={
                                     <ProtectedRoute>
                                         <Home />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="/room/:id"
-                                element={
-                                    <ProtectedRoute>
-                                        <Room />
                                     </ProtectedRoute>
                                 }
                             />
--- NaNowo/pg-pn-1820-developerex/client/src/pages/Home.tsx
+++ NaNowo/pg-pn-1820-developerex/client/src/pages/Home.tsx
@@ -1,84 +1,31 @@
-import { useAtomValue } from 'jotai';
-import { userAtom } from '../lib/user';
-import { serverUrl } from '../lib/data';
-import { useQuery } from '@tanstack/react-query';
-import RoomThumbnail from '../components/RoomThumbnail';
-import { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
-import AddRoomModal from '../components/AddRoomModal';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import image1 from '../pages/images/przybylo.png'
+import image2 from '../pages/images/frydrychkox.png'
+import image3 from '../pages/images/meszka1.jpg'
 
 const Home = () => {
-    const user = useAtomValue(userAtom);
-    const [data, setData] = useState<any[]>([]);
-    const [showModal, setShowModal] = useState(false);
-
-
-    const authTokenCookie = document.cookie
-                                .split('; ')
-                                .find(row => row.startsWith('authenticationToken='));
-
-    const authToken = authTokenCookie ? authTokenCookie.split('=')[1] : undefined;
-
-
-    // TODO: Fix types
-
-    const queryRooms = async ({ queryKey }: { queryKey: any }) => {
-
-        const res = await fetch(`${serverUrl}/api/rooms/${queryKey}/${user}`, {
-            method: 'GET',
-            headers: {
-              'Authorization': `Bearer ${authToken}`
-            },
-        });
-        if (!res.ok) throw new Error('Network response was not ok');
-
-        return res.json();
-    };
-
-    const queryOne = useQuery({
-        queryKey: ['get-participation-rooms'],
-        queryFn: queryRooms,
-    });
-
-    const queryTwo = useQuery({
-        queryKey: ['get-owned-rooms'],
-        queryFn: queryRooms,
-    });
-
-    useEffect(() => {
-        if (queryOne.data && queryTwo.data) {
-            setData([...queryOne.data, ...queryTwo.data]);
-        }
-    }, [queryOne.data, queryTwo.data]);
-
-    if (queryOne.isLoading || queryTwo.isLoading) return <p>Loading...</p>;
-    if (queryOne.error || queryTwo.error) return <p>Error :(</p>;
+    const tiles = [
+        { title: "Matematyka dyskretna", image: image3 },
+        { title: "Analiza matematyczna", image: image2 },
+        { title: "Algebra", image: image1 },
+    ];
 
     return (
-        <>
-            <Card className="w-1/2 border-0 shadow-none">
-                <CardHeader className="flex-row items-center justify-between">
-                    <CardTitle className="text-3xl">Your rooms</CardTitle>
-                    <Button onClick={() => setShowModal(true)}>
-                        Create Room
-                    </Button>
-                </CardHeader>
-                <CardContent>
-                    <ul>
-                        {data.map((room: any) => (
-                            <RoomThumbnail room={room} key={room.id} />
-                        ))}
-                    </ul>
-                </CardContent>
-            </Card>
-            {showModal &&
-                createPortal(
-                    <AddRoomModal onClose={() => setShowModal(false)} />,
-                    document.querySelector('#modal') as HTMLElement
-                )}
-        </>
+        <Card className="w-full sm:w-3/4 lg:w-1/2 border-0 shadow-none mx-auto">
+            <CardHeader className="flex-row items-center justify-between">
+                <CardTitle className="text-3xl text-center">Interactive Tiles</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    {tiles.map((tile, index) => (
+                        <div className="border border-gray-200 p-4 rounded-lg m-2 flex flex-col justify-start items-center" key={index}>
+                            <p>{tile.title}</p>
+                            <img src={tile.image} alt={tile.title} className="object-contain w-full h-full" />
+                        </div>
+                    ))}
+                </div>
+            </CardContent>
+        </Card>
     );
 };
 
