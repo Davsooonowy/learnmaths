a2773ab703e8bcb71de4ad1b54155ce2e29eca44
Dawid Mularczyk <muldaw@student.agh.edu.pl>
2024-01-21 16:00:10 +0100

    navbar ikonki itd

--- NaNowo/pg-pn-1820-developerex/client/src/components/Navbar.tsx
+++ NaNowo/pg-pn-1820-developerex/client/src/components/Navbar.tsx
@@ -2,59 +2,76 @@ import { useAtom } from 'jotai';
 import { userAtom } from '../lib/user';
 import { Link } from 'react-router-dom';
 import { ModeToggle } from './mode-toggle';
-import { Button, buttonVariants } from './ui/button';
 import { serverUrl } from '../lib/data';
+import { useTheme } from '@/components/theme-provider';
+import { Button as MuiButton, styled } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
 const Navbar = () => {
     const [user, setUser] = useAtom(userAtom);
+    const { theme } = useTheme(); // Get the current theme
 
     return (
-        <nav className="flex items-center justify-between px-6 py-4">
-            <span className="text-xl font-semibold">LearnMaths</span>
+        <nav className={`flex items-center justify-between px-6 py-4 ${theme === 'light' ? 'navbar-light' : ''}`}>
+            <span style={{ color: '#3F51B5' }} className="text-xl text-blue-500 font-semibold">LearnMaths</span>
             <ul className="flex items-center gap-6 font-medium">
                 <li>
-                    <Link
+                    <MuiButton
+                        variant="contained"
+                        color="primary"
+                        startIcon={<HomeIcon/>}
+                        component={Link}
                         to="/"
-                        className={buttonVariants({ variant: 'link' })}
                     >
                         Home
-                    </Link>
+                    </MuiButton>
                 </li>
-                {user== undefined && (
+                {user == undefined && (
                     <li>
-                        <a
-                            href="/login"
-                            className={buttonVariants({ variant: 'outline' })}
+                        <MuiButton
+                            variant="outlined"
+                            color="primary"
+                            startIcon={<VpnKeyIcon/>}
+                            component={Link}
+                            to="/login"
                         >
                             Login
-                        </a>
+                        </MuiButton>
                     </li>
                 )}
                 {user == undefined && (
                     <li>
-                        <a
-                            href="/register"
-                            className={buttonVariants({ variant: 'outline' })}
+                        <MuiButton
+                            variant="outlined"
+                            color="primary"
+                            startIcon={<PersonAddIcon/>}
+                            component={Link}
+                            to="/register"
                         >
                             SignUp
-                        </a>
+                        </MuiButton>
                     </li>
                 )}
                 {user != undefined && (
                     <li>
-                        <Button
-                            variant="outline"
+                        <MuiButton
+                            variant="outlined"
+                            color="secondary"
+                            startIcon={<ExitToAppIcon />}
                             onClick={() => {
-                                setUser(undefined );
+                                setUser(undefined);
                                 handleLogout();
                             }}
                         >
                             Logout
-                        </Button>
+                        </MuiButton>
                     </li>
                 )}
                 <li>
-                    <ModeToggle />
+                    <ModeToggle/>
                 </li>
             </ul>
         </nav>
